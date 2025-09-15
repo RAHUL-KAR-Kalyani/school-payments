08b772772c03755d4c3b7ee00c2a692cf30f656e
Rahul Kar <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2025-09-15 19:05:48 +0530

    solved issue during deploy

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: import.meta.env.BACKEND_URL,
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     headers: { 'Content-Type': 'application/json' }
 });
 
