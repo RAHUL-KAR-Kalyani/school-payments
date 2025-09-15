3e3eb48c512585f337e9f7a634651451d0b0c0a0
Rahul Kar <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2025-09-15 19:18:13 +0530

    removes unnessery test

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import transactionsReducer from './transactionsSlice';
-// import paymentReducer from "./paymentSlice";
 import authReducer from './authSlice';
 import uiReducer from './uiSlice';
 
 const store = configureStore({
 	reducer: {
 		transactions: transactionsReducer,
-		// payment: paymentReducer,
 		auth: authReducer,
 		ui: uiReducer
 	}
