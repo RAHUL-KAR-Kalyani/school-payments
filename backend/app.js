a82c79c60807c5f08c6c23e2bed9c0514bb73624
Rahul Kar <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2025-09-15 18:50:40 +0530

    add cors

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,10 @@ const transactionRoutes = require('./routes/transaction.routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
